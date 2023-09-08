3e27966b482682256c2baf1ef3a29f6da5e16712
Ednaira Viana <ednairaviana.evs@gmail.com>
2023-09-08 18:39:44 -0300

    Create countrySelectOnInput

--- src/form_components/contactInformation.js
+++ src/form_components/contactInformation.js
@@ -1,15 +1,35 @@
 import { listCountries } from "../countryObject";
 
+// DOM CONSTS
 const countrySelect = document.querySelector("#country");
+const phoneInput = document.querySelector("#phone");
+const zipInput = document.querySelector("#zip_code");
+
+// MAIN FUNCTION
 function setContactInformations() {
-    insertOption();
+  insertOption();
+  countrySelectOnInput();
+}
+
+// COMPONENTS FUNCTIONS
+
+function countrySelectOnInput() {
+  countrySelect.addEventListener("input", () => {
+    listCountries.forEach((element) => {
+      let country = element;
+      if (countrySelect.value === country.value) {
+        phoneInput.value = "";
+        console.log(country.value);
+      }
+    });
+  });
 }
 
 function insertOption() {
-    listCountries.forEach((country) => {
-      const countryOption = new Option(country.name, country.value);
-      countrySelect.add(countryOption);
-    });
+  listCountries.forEach((country) => {
+    const countryOption = new Option(country.name, country.value);
+    countrySelect.add(countryOption);
+  });
 }
 
-export {setContactInformations}
+export { setContactInformations };
