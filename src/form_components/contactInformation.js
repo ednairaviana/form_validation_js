9e045f8a9e04fd23d675930e23092187b1d051da
Ednaira Viana <ednairaviana.evs@gmail.com>
2023-09-08 19:20:05 -0300

    Create setPhoneConstraint

--- src/form_components/contactInformation.js
+++ src/form_components/contactInformation.js
@@ -19,10 +19,31 @@ function countrySelectOnInput() {
       let country = element;
       if (countrySelect.value === country.value) {
         phoneInput.value = "";
-        console.log(country.value);
+        setPhoneConstraint(country);
       }
     });
   });
+
+  function setPhoneConstraint(country) {
+    phoneInput.placeholder = country.phonePlaceholder;
+    phoneInput.addEventListener("keydown", (k) => {
+      const input = k;
+      const keycode = k.keyCode;
+      onlyNumbersKeydown(keycode, input);
+    });
+
+    phoneInput.addEventListener("input", () => {
+      const phoneValue = initInput(phoneInput);
+      country.phoneValidation(phoneValue, phoneInput);
+
+      const phoneRegex = new RegExp(country.phonePattern);
+      if (!phoneRegex.test(phoneInput.value)) {
+        phoneInput.setCustomValidity("Invalid");
+      } else {
+        phoneInput.setCustomValidity("");
+      }
+    });
+  }
 }
 
 function insertOption() {
@@ -30,6 +51,28 @@ function insertOption() {
     const countryOption = new Option(country.name, country.value);
     countrySelect.add(countryOption);
   });
+}
+
+function initInput(input) {
+  let string = input.value;
+  string = string.replace(/[()-\s]/g, "");
+
+  input.value = string;
+  return string;
+}
+
+function onlyNumbersKeydown(keycode, input) {
+  let digits = [8, 3, 39, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+
+  let digit;
+  digits.forEach((el) => {
+    if (keycode === el) {
+      digit = keycode;
+    }
+  });
+  if (keycode !== digit || input.shiftKey) {
+    input.preventDefault();
+  }
 }
 
 export { setContactInformations };
