34cb824ffeadcb2d76d3141f4cdaf6737db38150
Ednaira Viana <ednairaviana.evs@gmail.com>
2023-09-08 17:11:47 -0300

    First Commit

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("O James, eu quero uma salada de fruta"),console.log("Batatinha"),console.log("Tomanocu"),console.log("Alguma coisa")})();
---
+++ src/test.js
@@ -0,0 +1,8 @@
+function teste() {
+    console.log("O James, eu quero uma salada de fruta")
+    console.log("Batatinha")
+    console.log("Tomanocu")
+    console.log("Alguma coisa")
+}
+
+export {teste}
