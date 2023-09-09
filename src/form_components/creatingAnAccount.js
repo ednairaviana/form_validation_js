1629f3746b12926567b6657bfc44a496469bef70
Ednaira Viana <ednairaviana.evs@gmail.com>
2023-09-09 18:51:03 -0300

    create custom validity to checkEmail

--- src/form_components/creatingAnAccount.js
+++ src/form_components/creatingAnAccount.js
@@ -1,5 +1,6 @@
 import { emailList } from "./emailList";
 import { pwCharList } from "./charList";
+import { insertPopUp, okMessage } from "../dom_components/createPopup";
 
 const emailInput = document.querySelector("#email");
 const pwInput = document.querySelector("#password");
@@ -15,18 +16,29 @@ function checkEmail() {
   emailInput.addEventListener("input", () => {
     const emailArray = emailInput.value.split("@");
 
-    if (emailArray.length == 2) {
-      if (emailList.includes(emailArray[1])) {
-        emailInput.setCustomValidity("");
+    if (emailArray.length < 2) {
+      if (emailArray[0].length < 3) {
+        emailInput.setCustomValidity("Invalid");
+        insertPopUp(emailInput, "Too short!");
+        return;
+      } else if (emailArray[0].length > 50) {
+        emailInput.setCustomValidity("Invalid");
+        insertPopUp(emailInput, "Too long!");
+        return;
       } else {
-        emailInput.setCustomValidity("Invalid!");
+        emailInput.setCustomValidity("Invalid");
+        insertPopUp(emailInput, "Don't forget @!");
         return;
       }
-    } else if (emailArray[0].length < 3 || emailArray[0].length > 50) {
-      emailInput.setCustomValidity("Invalid");
-      return;
-    } else {
-      emailInput.setCustomValidity("");
+    } else if (emailArray.length == 2) {
+      if (!emailList.includes(emailArray[1])) {
+        emailInput.setCustomValidity("Invalid!");
+        insertPopUp(emailInput, "Your host name is not valid!");
+      } else {
+        emailInput.setCustomValidity("");
+        okMessage(emailInput);
+        return;
+      }
     }
   });
 }
