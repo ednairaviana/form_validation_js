dca863c9c7def0c3c3936e73558dc1b850134962
Ednaira Viana <ednairaviana.evs@gmail.com>
2023-09-09 01:11:53 -0300

    Create checkPassword and checkConfirmPassword

--- src/form_components/creatingAnAccount.js
+++ src/form_components/creatingAnAccount.js
@@ -1,4 +1,5 @@
 import { emailList } from "./emailList";
+import { pwCharList } from "./charList";
 
 const emailInput = document.querySelector("#email");
 const pwInput = document.querySelector("#password");
@@ -6,6 +7,8 @@ const confirmPwInput = document.querySelector("#confirm_pw");
 
 function setCreatingAccount() {
   checkEmail();
+  checkPassword();
+  checkConfimrPassword();
 }
 
 function checkEmail() {
@@ -26,6 +29,80 @@ function checkEmail() {
       emailInput.setCustomValidity("");
     }
   });
+}
+
+function checkPassword() {
+  pwInput.addEventListener("input", () => {
+    const password = pwInput.value.split("");
+
+    if (password.includes(" ")) {
+      pwInput.setCustomValidity("Invalid!");
+      return;
+    }
+    if (password.length < 10) {
+      pwInput.setCustomValidity("Invalid!");
+      return;
+    }
+    if (!checkAlpha(password)) {
+      pwInput.setCustomValidity("Invalid!");
+      return;
+    }
+    if (!checkNumber(password)) {
+      pwInput.setCustomValidity("Invalid!");
+      return;
+    }
+    if (!checkSymbol(password)) {
+      pwInput.setCustomValidity("Invalid!");
+      return;
+    }
+
+    pwInput.setCustomValidity("");
+  });
+}
+
+function checkConfimrPassword() {
+  confirmPwInput.addEventListener("input", () => {
+    if (pwInput.value !== confirmPwInput.value) {
+      confirmPwInput.setCustomValidity("Invalid!");
+    } else {
+      confirmPwInput.setCustomValidity("");
+    }
+  });
+}
+
+// COMPONENT FUNCTIONS
+
+function checkAlpha(password) {
+  let validity = false;
+  pwCharList.alpha.forEach((char) => {
+    if (password.includes(char)) {
+      validity = true;
+    }
+  });
+
+  return validity;
+}
+
+function checkNumber(password) {
+  let validity = false;
+  pwCharList.numbers.forEach((char) => {
+    if (password.includes(char)) {
+      validity = true;
+    }
+  });
+
+  return validity;
+}
+
+function checkSymbol(password) {
+  let validity = false;
+  pwCharList.symbols.forEach((char) => {
+    if (password.includes(char)) {
+      validity = true;
+    }
+  });
+
+  return validity;
 }
 
 export { setCreatingAccount };
